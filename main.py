6085044bb06d22136563f9c18ec62b2fa7b4ed0a
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-06-03 22:49:40 +0000

    gua

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+menu 
+cambiar los jugos
+modificar agregar y eliminar  ingredientes 
+tiempo
+mostrar
+salir
+"""
+
+#bucle 
+
+def main ():
+    while True:
+        opcion = input("opcion")
+
+        if opcion =="1":
+            print("1")
+
+        elif opcion == "2":
+            print("2")
+
+        elif opcion == "3":
+            print("3")
+
+        elif opcion == "4":
+            print("4")
+
+        elif opcion 
+
